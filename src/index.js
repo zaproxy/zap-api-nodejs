b023a585b50e5279cd7af6e24aaaf087b567b997
Kim Carter <Kim.Carter@binarymist.net>
2018-10-29 20:57:23 +1300

    Added package.json and index.js

---
+++ src/index.js
@@ -0,0 +1,164 @@
+/* Zed Attack Proxy (ZAP) and its related class files.
+ *
+ * ZAP is an HTTP/HTTPS proxy for assessing web application security.
+ *
+ * Copyright the ZAP development team
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+const request = require('request');
+const requestPromise = require('request-promise-native');
+const _ = require('lodash');
+
+
+const Acsrf = require('./acsrf');
+const AjaxSpider = require('./ajaxSpider');
+const Ascan = require('./ascan');
+const Authentication = require('./authentication');
+const Authorization = require('./authorization');
+const Autoupdate = require('./autoupdate');
+const Brk = require('./brk');
+const Context = require('./context');
+const Core = require('./core');
+const ForcedUser = require('./forcedUser');
+const HttpSessions = require('./httpSessions');
+const ImportLogFiles = require('./importLogFiles');
+const Params = require('./params');
+const Pnh = require('./pnh');
+const Pscan = require('./pscan');
+const Script = require('./script');
+const Search = require('./search');
+const SessionManagement = require('./sessionManagement');
+const Spider = require('./spider');
+const Stats = require('./stats');
+const Users = require('./users');
+
+// base JSON api url
+const BASE = 'http://zap/JSON';
+// base OTHER api url
+const BASE_OTHER = 'http://zap/OTHER';
+
+function ClientApi(options) {
+  options = options || {};
+  _.defaults(options, {
+    proxy: 'http://127.0.0.1:8080'
+  });
+  
+  if (!options.apiKey) throw new Error('An apiKey must be provided for the Zap API.');
+
+  const requestOptions = {
+    method: 'GET',
+    json: true,
+    apiKey: options.apiKey,
+    proxy: options.proxy
+  };
+
+  this.req = request.defaults(requestOptions);
+  this.reqPromise = requestPromise.defaults(requestOptions);
+  this.acsrf = new Acsrf(this);
+  this.ajaxSpider = new AjaxSpider(this);
+  this.ascan = new Ascan(this);
+  this.authentication = new Authentication(this);
+  this.authorization = new Authorization(this);
+  this.autoupdate = new Autoupdate(this);
+  this.brk = new Brk(this);
+  this.context = new Context(this);
+  this.core = new Core(this);
+  this.forcedUser = new ForcedUser(this);
+  this.httpSessions = new HttpSessions(this);
+  this.importLogFiles = new ImportLogFiles(this);
+  this.params = new Params(this);
+  this.pnh = new Pnh(this);
+  this.pscan = new Pscan(this);
+  this.script = new Script(this);
+  this.search = new Search(this);
+  this.sessionManagement = new SessionManagement(this);
+  this.spider = new Spider(this);
+  this.stats = new Stats(this);
+  this.users = new Users(this);
+}
+
+// Legacy for callbacks.
+
+/**
+ * Get a handler for REST API responses.
+ * We include a workaround here for the fact that the API does not
+ * return the correct status codes in the event of an error
+ * (i.e. it always returns 200).
+ **/
+const responseHandler = function (callback) {
+  return function handleResponse(err, res, body) {
+    if (err) {
+      callback(err);
+      return;
+    }
+
+    // if the response has a code and a message, it's an error.
+    if (body && body.code && body.message) {
+      callback(body);
+    } else {
+      callback(null, body);
+    }
+  };
+};
+
+ClientApi.prototype.request = function (url, parms, callback) {
+  if (!callback && typeof(parms === 'function')) {
+    callback = parms;
+    parms = null;
+  }
+
+  var options = {
+    url: BASE + url
+  };
+  if (parms) {
+    options.qs = parms;
+  }
+  this.req(options, responseHandler(callback));
+};
+
+ClientApi.prototype.requestOther = function (url, parms, callback) {
+  if (!callback && typeof(parms === 'function')) {
+    callback = parms;
+    parms = null;
+  }
+
+  var options = {
+    url: BASE_OTHER + url
+  };
+  if (parms) {
+    options.qs = parms;
+  }
+  this.req(options, responseHandler(callback));
+};
+
+// End Legacy for callbacks.
+
+const makeRequest = function (parms, options) {
+  return this.reqPromise(parms ? { ...options, qs: parms } : options);
+};
+
+
+ClientApi.prototype.requestPromise = function (url, parms) {
+  return makeRequest.call(this, parms, { url: BASE + url });
+};
+
+
+ClientApi.prototype.requestPromiseOther = function (url, parms) {
+  return makeRequest.call(this, parms, { url: BASE_OTHER + url });
+};
+
+module.exports = ClientApi;
