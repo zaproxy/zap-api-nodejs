bd9eb9d6f8123f943f64c1b248c0acf1fea46312
Najam Ul Saqib <najamulsaqib@tutamail.com>
2023-03-24 23:55:15 -0700

    Update index.js
    Fix variable assignment, config does not exist.
    
    Signed-off-by: Najam Ul Saqib <najamulsaqib@tutamail.com>

--- src/index.js
+++ src/index.js
@@ -111,11 +111,11 @@ ClientApi.prototype.request = async (url, data, format) => {
   try {
     let requestConfig = defaultAxiosConfig;
     if (data) {
-      config.params = { ...config.params, ...data };
+      requestConfig.params = { ...requestConfig.params, ...data };
     }
     let response;
     if (format === 'other') {
-      config = { ...config, baseURL: BASE_URL_OTHER };
+      requestConfig = { ...requestConfig, baseURL: BASE_URL_OTHER };
     }
     response = await axios.get(url, requestConfig);
 
